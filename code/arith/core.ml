5b421df40878be4bc9a3af4c610efe54c4d7b7bf
Alve <>
2026-01-31 11:57:52 +0100

    Add code solutions for chapter 4

--- code/arith/core.ml
+++ code/arith/core.ml
@@ -50,3 +50,42 @@ let rec eval t =
   try let t' = eval1 t
       in eval t'
   with NoRuleApplies -> t
+
+(* begin snippet 421a
+let rec eval t =
+  let eval1' t =
+    try Some(eval1 t)
+    with NoRuleApplies -> None in
+  match eval1' t with
+      Some(t') -> eval t'
+    | None -> t
+end snippet *)
+
+(* begin snippet 421b
+let eval t =
+  let t = ref t in
+  try while true do
+    t := eval1 !t
+  done
+  with NoRuleApplies -> ();
+  !t
+end snippet *)
+
+(* begin snippet 422
+let rec eval t = match t with
+    v when isval v -> v
+  | TmIf(_,t1,t2,t3) -> begin match eval t1 with
+      TmTrue(_) -> eval t2
+    | TmFalse(_) -> eval t3
+    | _ -> raise NoRuleApplies end
+  | TmSucc(fi,t1) -> TmSucc(fi,eval(t1))
+  | TmPred(_,t1) -> begin match eval t1 with
+      TmZero(fi) -> TmZero(fi)
+    | TmSucc(_,nv1) when isnumericalval nv1 -> nv1
+    | _ -> raise NoRuleApplies end
+  | TmIsZero(_,t1) -> begin match eval t1 with
+      TmZero(fi) -> TmTrue(dummyinfo)
+    | TmSucc(_,nv1) when isnumericalval nv1 -> TmFalse(dummyinfo)
+    | _ -> raise NoRuleApplies end
+  | _ -> raise NoRuleApplies (* Unreachable *)
+end snippet *)
